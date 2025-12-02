70d89e0646eb425113fd98a0563be45aefcd9ed3
Bryce Thuilot <bryce@thuilot.io>
2025-12-02 12:12:10 -0500

    feat(day 2): complete day 2
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

---
+++ src/Utils/Strings.hs
@@ -0,0 +1,20 @@
+{-
+ Copyright (C) 2025 Bryce Thuilot
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the FSF, either version 3 of the License, or (at your option) any later version.
+ See the LICENSE file in the root of this repository for full license text or
+ visit: <https://www.gnu.org/licenses/gpl-3.0.html>.
+-}
+
+module Utils.Strings (wordsWhen) where
+
+-- | 'wordsWhen' is the same as 'words' but the predicate
+-- can be passed in. Take from StackOverflow
+-- https://stackoverflow.com/a/4981265
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,13 +8,11 @@
  visit: <https://www.gnu.org/licenses/gpl-3.0.html>.
 -}
 
-
-import Data.Maybe
-import Data.List
-import System.Exit
-
-import Days
 import AOC
+import Data.List
+import Data.Maybe
+import Days
+import System.Exit
 
 main :: IO ()
 main = do
@@ -26,60 +24,84 @@ main = do
 
 testCases :: [TestCase]
 testCases =
-  [ TestCase{ day = 0
-            , part = 1
-            , input = "hello, world!"
-            , expected = "hello, world!"
-            }
-  , TestCase{ day = 0
-            , part = 2
-            , input = "hello, world!"
-            , expected = "HELLO, WORLD!"
-            }
-  , TestCase{ day = 1
-            , part = 1
-            , input = "L68\n" ++
-                      "L30\n" ++
-                      "R48\n" ++
-                      "L5\n" ++
-                      "R60\n" ++
-                      "L55\n" ++
-                      "L1\n" ++
-                      "L99\n" ++
-                      "R14\n" ++
-                      "L82\n"
-            , expected = "3"
-            }
-  , TestCase{ day = 1
-            , part = 2
-            , input = "L68\n" ++
-                      "L30\n" ++
-                      "R48\n" ++
-                      "L5\n" ++
-                      "R60\n" ++
-                      "L55\n" ++
-                      "L1\n" ++
-                      "L99\n" ++
-                      "R14\n" ++
-                      "L82\n"
-            , expected = "6"
-            }
-      , TestCase{ day = 1
-            , part = 2
-            , input = "R1000\n"
-            , expected = "10"
-            }
+  [ TestCase
+      { day = 0,
+        part = 1,
+        input = "hello, world!",
+        expected = "hello, world!"
+      },
+    TestCase
+      { day = 0,
+        part = 2,
+        input = "hello, world!",
+        expected = "HELLO, WORLD!"
+      },
+    TestCase
+      { day = 1,
+        part = 1,
+        input =
+          "L68\n"
+            ++ "L30\n"
+            ++ "R48\n"
+            ++ "L5\n"
+            ++ "R60\n"
+            ++ "L55\n"
+            ++ "L1\n"
+            ++ "L99\n"
+            ++ "R14\n"
+            ++ "L82\n",
+        expected = "3"
+      },
+    TestCase
+      { day = 1,
+        part = 2,
+        input =
+          "L68\n"
+            ++ "L30\n"
+            ++ "R48\n"
+            ++ "L5\n"
+            ++ "R60\n"
+            ++ "L55\n"
+            ++ "L1\n"
+            ++ "L99\n"
+            ++ "R14\n"
+            ++ "L82\n",
+        expected = "6"
+      },
+    TestCase
+      { day = 1,
+        part = 2,
+        input = "R1000\n",
+        expected = "10"
+      },
+    TestCase
+      { day = 2,
+        part = 1,
+        input =
+          "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,"
+            ++ "1698522-1698528,446443-446449,38593856-38593862,565653-565659,"
+            ++ "824824821-824824827,2121212118-2121212124",
+        expected = "1227775554"
+      },
+    TestCase
+      { day = 2,
+        part = 2,
+        input =
+          "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,"
+            ++ "1698522-1698528,446443-446449,38593856-38593862,565653-565659,"
+            ++ "824824821-824824827,2121212118-2121212124",
+        expected = "4174379265"
+      }
   ]
 
 data TestCase = TestCase
-  { day :: Int
-  , part :: Int
-  , input :: String
-  , expected :: String
+  { day :: Int,
+    part :: Int,
+    input :: String,
+    expected :: String
   }
 
 data TestResult = TestPass | TestFail String | TestSkippedNotImplemeted | TestSkippedNoInput
-
 
 printTestResult :: Int -> Int -> TestResult -> IO ()
 printTestResult dayNum partNum tr = putStrLn output
@@ -89,7 +111,6 @@ printTestResult dayNum partNum tr = putStrLn output
     showResult TestSkippedNotImplemeted = "skipped - not implemeted"
     showResult TestSkippedNoInput = "skipped - no input"
     showResult (TestFail s) = "failed - " ++ s
-
 
 runTestCase :: TestCase -> IO TestResult
 runTestCase tc = do
@@ -102,5 +123,3 @@ runTestCase tc = do
     evalResult (Ok output) = if e == output then TestPass else TestFail (output ++ " did not match expected " ++ e)
     evalResult NotImplemented = TestSkippedNotImplemeted
     evalResult _ = TestFail "unable to run"
-
-  
